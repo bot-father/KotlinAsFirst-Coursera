b0293ea353a38e69b7052553ebbbca39e77b8dee
bot-father <trucker.diesel@gmail.com>
2019-04-07 10:47:53 +0200

    Debug

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -67,7 +67,7 @@ fun ageDescription(age: Int): String =
         age > 4 && age < 21 -> "$age лет"
         age % 10 > 1 && age % 10 < 5 -> "$age года"
         age % 10 > 4 -> "$age лет"
-        age % 10 == 1 -> "$age год"
+        else -> "$age год"
     }
 
 /**
